5e0c5e9167d674389bde83c723e0ebb181a96d77
R2D2 <66644638+R2rit0@users.noreply.github.com>
2021-11-25 11:20:04 -0400

    fix: SessionData is now properly working on Request.session

--- src/@types/globalTypes.ts
+++ src/@types/globalTypes.ts
@@ -1,9 +1,20 @@
 import { Types } from "mongoose";
 
-declare module "express-session" {
-  export interface SessionData {
-    restaurantId: Types.ObjectId;
-    tableId: number;
-    order?: [{ productId: Types.ObjectId; quantity: number }];
+// declare module "express-session" {
+//   export interface SessionData {
+//     restaurantId: Types.ObjectId;
+//     tableId: number;
+//     order?: [{ productId: Types.ObjectId; quantity: number }];
+//   }
+// }
+
+declare global {
+  namespace Express {
+    interface Session {
+      restaurantId: string;
+      tableId: number;
+      order?: [{ productId: Types.ObjectId; quantity: number }];
+      userId: string;
+    }
   }
 }
